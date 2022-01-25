d61b83702bd1b65bc76c6cecf3ea5e94ba2c4f34
杨志强 <yangzhiqiang@winwin-inc.com>
2022-01-25 15:04:24 +0800

    perf: update

--- tools/demo/pages/preview/index.js
+++ tools/demo/pages/preview/index.js
@@ -1,3 +1,9 @@
+/**
+ * @file 预览组件使用
+ * @module pages/preview/index
+ * @author 月落 <yueluo.yang@qq.com>
+ */
+
 Page({
   /**
    * @property {array} imgs - 图片列表
@@ -16,9 +22,18 @@ Page({
     ]
   },
 
+  // 预览组件引用
+  previewRef: null,
+
+  onLoad() {
+    this.init();
+  },
+
+  async init() {
+  },
+
   /**
    * @description 预览单张图片
-   * @param {object} e - 事件对象
    * @returns {void}
    */
   handlePreviewSingle(e) {
@@ -37,6 +52,7 @@ Page({
 
   /**
    * @description 预览组件下标改变
+   * @returns {void}
    */
   handlePreviewChange({ current }) {
     console.log(current);
