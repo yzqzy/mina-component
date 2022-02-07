78dffe4127f35e8ffb1d65db909826782a5bd8fe
杨志强 <yangzhiqiang@winwin-inc.com>
2022-02-07 10:43:57 +0800

    feat: 图片预览组件处理完成

--- tools/demo/pages/preview/index.js
+++ tools/demo/pages/preview/index.js
@@ -7,19 +7,35 @@
 Page({
   /**
    * @property {array} imgs - 图片列表
+   * @property {number} current - 当前预览选中下标
+   * @property {string} selected - 当前预览的组
+   * @property {object} imgsHelper - 收藏状态
    */
   data: {
     imgs: [
       {
+        id: 1,
         url: 'https://data.yueluo.club/git.jpg'
       },
       {
+        id: 2,
         url: 'https://data.yueluo.club/leetcode.png'
       },
       {
+        id: 3,
         url: 'https://data.yueluo.club/es6.jpg'
       }
-    ]
+    ],
+
+    current: 0,
+
+    selected: 'base', // base、slot
+
+    imgsHelper: {
+      1: {
+        focus: true
+      }
+    }
   },
 
   // 预览组件引用
@@ -38,12 +54,16 @@ Page({
    * @returns {void}
    */
   handlePreviewSingle(e) {
-    const { index } = e.currentTarget.dataset;
+    const { index, field } = e.currentTarget.dataset;
     const urls = this.data.imgs.map(item => item.url);
 
     this.previewRef.show({
       current: index,
       urls
+    });
+    this.setData({
+      current: index,
+      selected: field
     });
   },
 
@@ -61,6 +81,43 @@ Page({
    */
   handlePreviewChange(e) {
     const { current } = e.detail;
-    console.log(current);
+
+    this.setData({
+      current
+    });
+  },
+
+  /**
+   * @description 修改收藏状态
+   * @returns {void}
+   */
+  hanleSwitchFocusStatus(e) {
+    const { current } = e.currentTarget.dataset;
+    const { imgsHelper } = this.data;
+
+    if (!imgsHelper[current]) {
+      imgsHelper[current] = { focus: false };
+    }
+
+    const { focus } = imgsHelper[current];
+    const content = focus ? '您确认取消收藏该图片吗？' : '您确认收藏该图片吗？';
+
+    wx.showModal({
+      content: content,
+      confirmColor: "#457EFC",
+      confirmText: "确定",
+      cancelColor: "#999999",
+      cancelText: "取消",
+      success: (res) => {
+        if (res.confirm) {
+          this.setData({
+            [`imgsHelper[${current}]`]: {
+              focus: !focus
+            }
+          });
+        }
+      }
+    });
+
   }
 });
