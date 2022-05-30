871f780b4d579ad6d6b1e4574f0168645d44d34a
yueluo <yueluo.yang@qq.com>
2022-05-30 18:11:16 +0800

    fix: 表格高度计算错误

--- packages/pro-table/shared/tool.ts
+++ packages/pro-table/shared/tool.ts
@@ -26,7 +26,7 @@ export const normalizeColumns = (originColumns) => {
 
   return {
     leftColumns,
-    middleColumns
+    middleColumns,
   };
 };
 
@@ -48,32 +48,33 @@ const getMultiiple = (text) => (/^\d+$/.test(text) ? 2 : 1);
  * @property {Number} params.rowH - 行高
  * @returns {Object}
  */
-export const normalizeDataSource = (
-  {
-    leftColumns = [] as any[],
-    middleColumns = [] as any[],
-    dataSource = [] as any[],
-    hd = 64,
-    wordLimit = 8,
-    rowH = 0
-  }
-) => {
+export const normalizeDataSource = ({
+  leftColumns = [] as any[],
+  middleColumns = [] as any[],
+  dataSource = [] as any[],
+  hd = 64,
+  wordLimit = 8,
+  rowH = 0,
+}) => {
   if (!dataSource.length) {
     return {
       height: hd,
-      dataSource
+      dataSource,
     };
   }
 
   const increase = Math.floor(rowH / 3);
 
-  middleColumns = middleColumns.filter(col => col.tooltip) || [];
+  middleColumns = middleColumns.filter((col) => col.tooltip) || [];
 
   const height = dataSource.reduce((h, item, index) => {
     let maxH = rowH;
 
     middleColumns.forEach((col) => {
-      const { dataIndex, tooltip: { limit } } = col;
+      const {
+        dataIndex,
+        tooltip: { limit },
+      } = col;
       const field = item[dataIndex] || '';
 
       if (field.length > limit) {
@@ -82,7 +83,7 @@ export const normalizeDataSource = (
       }
     }, []);
 
-    leftColumns.forEach(col => {
+    leftColumns.forEach((col) => {
       let field = item[col.dataIndex] || '';
       let divMultiple = getMultiiple(field);
 
@@ -91,16 +92,19 @@ export const normalizeDataSource = (
         field += item[col.sub.dataIndex];
       }
 
-      let multiple = Math.floor(field.length / (wordLimit * divMultiple));
+      let multiple = field.length / (wordLimit * divMultiple);
 
       if (col.extra && col.extra.merge) {
-        const mergeArr = Array.isArray(col.extra.merge) ? col.extra.merge : [col.extra.merge];
+        const mergeArr = Array.isArray(col.extra.merge)
+          ? col.extra.merge
+          : [col.extra.merge];
 
         mergeArr.forEach(({ dataIndex, limit }) => {
           const mergeField = item[dataIndex];
 
           if (mergeField) {
-            const currMultiple = Math.floor(mergeField.length / (limit * getMultiiple(mergeField)));
+            const currMultiple =
+              mergeField.length / (limit * getMultiiple(mergeField));
 
             multiple = Math.max(multiple, currMultiple);
           }
@@ -113,10 +117,10 @@ export const normalizeDataSource = (
     });
 
     return h + maxH;
-  }, rowH);
+  }, hd);
 
   return {
     height,
-    list: dataSource
+    list: dataSource,
   };
 };
